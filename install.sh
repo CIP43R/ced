6e538757b2ae57d661a8e75d40ef3dddd48413f9
Justin Falkenstein <justin.falkenstein@outlook.com>
2023-01-17 18:06:11 +0100

    fix(shell): yeah, no, not sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt install git -y
-mkdir -p /opt/setup-linux-server
+mkdir -p /opt/setup-linux-server 2>&1
 cd /opt/setup-linux-server
 git clone https://github.com/CIP43R/setup-linux-server.git
-sh setup-linux-server/init.sh
+/opt/setup-linux-server/init.sh
