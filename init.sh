847d8b99e231f773f3fc05282f01cdad369eb20a
Justin Falkenstein <justin.falkenstein@outlook.com>
2023-01-17 16:57:51 +0100

    fix(ssh): auth
    fixed auth for 2fa google authentication

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# shellcheck disable=SC2059 disable=SC2181 disable=SC2154
 # init.sh
 # Script to setup a linux server with some basic apps and configs
 
@@ -10,7 +9,8 @@ cwd=$(pwd)
 logloc=$cwd/logs/install.log # Custom logs produced in this file
 aptlogloc=$cwd/logs/apt.log  # Logs produced by apt 
 cmdlogloc=$cwd/logs/cmd.log  # Logs produced by applications run in here
-cprompt=false
+confirmed=false              # Check for prompt
+compared=false               # Check for string comparison in files
 
 ###
 # Colors
@@ -35,6 +35,7 @@ log() {
     ERROR)
       printf "${RED}[ERROR] $1\\n" | tee -a $logloc
       exit
+      ;;
   esac
   sleep 0.5
 }
@@ -42,11 +43,22 @@ confirm() {
   read -r -p "${1} [y/N] " response
   if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
   then
-      cprompt=true
+      confirmed=true
   else
-      cprompt=false
+      confirmed=false
   fi
 }
+compare() {
+  if grep -wq "$1" $2; then 
+    compared=true
+  else 
+    compared=false
+  fi
+}
+replace_config() {
+  sed -i "s/$1/$2" $3
+}
+
 
 ###
 # Installer functions
@@ -64,55 +76,59 @@ install_docker() {
   docker run hello-world || {
     printf "Docker hello world container was not booted correctly. You need to check the installation and try again" ERROR
   }
-  log "Created user $adminusr with sudo role" INFO
+  log "Installed docker successfully" INFO
 
 }
 # Install webmin
 install_webmin() {
   wget http://prdownloads.sourceforge.net/webadmin/webmin_2.011_all.deb
-  dpkg --install webmin_2.011_all.deb &> $aptlogloc || {
-    apt install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip shared-mime-info
-    dpkg --install webmin_2.011_all.deb || {
+  dpkg --install webmin_2.011_all.deb &> $cmdlogloc || {
+    apt install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip shared-mime-info -y &> $aptlogloc
+    dpkg --install webmin_2.011_all.deb &> $cmdlogloc || {
       log "Something went wrong while installing webmin." ERROR
     }
   }
+  log "Installed webmin successfully" INFO
 }
 # Nginx
 install_nginx() {
   apt install nginx -y &> $aptlogloc
-  echo "Paste your nginx reverse proxy config here: "
-  readvar nginxconf
-  echo $nginxconf >> /etc/nginx/sites-available/reverse-proxies.conf
-  ln -s /etc/nginx/sites-available/* /etc/nginx/sites-enabled/
-  nginx -t || {
-    log "There is an issue with the nginx config. Aborting..." ERROR
-  }
-  log "Installed and configured provided nginx configs in ${BLUEBG}/etc/nginx/sites-available/reverse-proxies.conf"
+  confirm "Installed nginx. Would you like to create a reverse proxy configuration right now?"
+  if [[ $confirmed = true ]]; then 
+    nano /etc/nginx/sites-available/reverse-proxies.conf
+    rm /etc/nginx/sites-available/default
+    ln -s /etc/nginx/sites-available/* /etc/nginx/sites-enabled
+    nginx -t || {
+      log "There is an issue with the nginx config. Aborting..." ERROR
+    }
+  fi
+  log "Installed and configured nginx successfully"
 }
-
-###
-# Initial setup
-###
-init_setup() {
-  {  
-    touch $cwd/logs/install.log
-    touch $cwd/logs/full.log
-    apt update && apt upgrade -y
-  } &> $aptlogloc
-  log "Updated system packages" INFO
+backup() {
   # Make a netplan backup to prevent network config issues
   mkdir -p $cwd/backups/netplan
   cp /etc/netplan/* $cwd/backups/netplan/
   log "Created a copy of netplan config in $cwd/backups/netplan" INFO
 }
-init_setup
+
+###
+# Initial setup
+###
+log "Installing updates...this may take a while" INFO
+{  
+  touch $cwd/logs/install.log
+  touch $cwd/logs/full.log
+  apt update && apt upgrade -y
+} &> $aptlogloc
+log "Updated system packages" INFO
+backup
 
 ###
 # Create central admin user and login as them
 ###
-adminusr=$user
-confirm "Do you want to create a central sudo user? This is recommended if only root is available."
-if [[ cprompt == true ]]; then
+adminusr=$(who am i | awk '{print $1}')
+confirm "Do you want to create a new central sudo user? This is recommended if only root is available."
+if [[ $confirmed = true ]]; then
   echo "Please type in a name for the new admin user: "
   read adminusr
   {
@@ -121,8 +137,8 @@ if [[ cprompt == true ]]; then
   } &> $cmdlogloc
   echo "Fill in a password for the new user: "
   read adminpwd
-  usermod --password $adminpwd $adminusr &> $logloc
-  unset adminpwd
+  usermod --password $adminpwd $adminusr &> $cmdlogloc
+  unset adminpwd # for security
   cd $cwd # back to the roots
   log "Created user $adminusr with sudo role" INFO
 fi
@@ -133,43 +149,67 @@ fi
 # Install ufw and allow ssh for next step
 apt install ufw -y &> $aptlogloc
 ufw allow ssh &> $cmdlogloc
-echo "y" | ufw --force enable
+echo "y" | ufw --force enable &> $cmdlogloc
 log "Installed and enabled ufw firewall" INFO
 # Configure SSH
 echo "Fill in RSA pub key: "
 read adminkey
-echo $adminkey >> /home/$adminusr/.ssh/authorized_keys
+compare "$adminkey" /home/$adminusr/.ssh/authorized_keys
+if [[ $compared = true ]]; then
+  log "SSH key already present. Skipping." INFO
+else
+  echo $adminkey >> /home/$adminusr/.ssh/authorized_keys
+fi
 yes | cp $cwd/ssh/sshd_config /etc/ssh/sshd_config
-echo "AllowUsers $adminusr" >> /etc/ssh/sshd_config
+allowusers="AllowUsers $adminusr"
+compare "$allowusers" /etc/ssh/sshd_config
+if [[ $compared = true ]]; then
+  log "User already in allowed section. Skipping." INFO
+else
+  echo $allowusers >> /etc/ssh/sshd_config
+fi
+log "Configured sshd to use RSA authentication only with provided key" INFO
 # Optionally configure 2FA
-echo "Enable 2FA (google authenticator?) [y/n]: "
-read tfa
-if [[ $tfa = "y" ]]; then
+confirm "Enable 2FA (google authenticator?)"
+if [[ $confirmed = true ]]; then
   apt install libpam-google-authenticator -y &> $aptlogloc
   echo "Scan the following QR code with the google authenticator app and insert the code when prompted."
-  google-authenticator -t -d
-  echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd
+  sudo -u $adminusr google-authenticator -t -d -W -f --step-size=30 --rate-limit=2 --rate-time=30
+
+  # Add google authenticator to pam config if not present
+  compare "auth required pam_google_authenticator.so" /etc/pam.d/sshd
+  if [[ $compared = true ]]; then
+    log "Google authenticator already configured for PAM. Skipping..." INFO
+  else
+    echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd
+  fi
+  replace_config "@include common-auth" "#@include common-auth" /etc/pam.d/sshd
+  replace_config "AuthenticationMethods publickey,password publickey" "AuthenticationMethods publickey,password publickey,keyboard-interactive" /etc/ssh/sshd_config
+  replace_config "UsePAM no" "UsePAM yes" /etc/ssh/sshd_config
+  log "Set up google authenticator for 2FA authentication" INFO
+else
+  replace_config "AuthenticationMethods publickey,password publickey,keyboard-interactive" "AuthenticationMethods publickey,password publickey" /etc/ssh/sshd_config
+  replace_config "UsePAM yes" "UsePAM no" /etc/ssh/sshd_config
+  replace_config "#@include common-auth" "@include common-auth" /etc/pam.d/sshd
 fi
-service sshd restart
-log "Configured sshd to use RSA $( if [[ $tfa = "y" ]]; then echo and 2FA fi ) authentication only with provided key" INFO
+systemctl restart sshd.service
 # Limit crontab to new root user
 echo $adminusr >> /etc/cron.d/cron.allow
 log "Limited usage of crontab to user $adminusr." INFO
 # Install and setup fail2ban with custom config
-apt install fail2ban -y
+apt install fail2ban -y &> $aptlogloc
 yes | cp $cwd/fail2ban/jail.local /etc/fail2ban/jail.local
 yes | cp $cwd/fail2ban/fail2ban.local /etc/fail2ban/fail2ban.local
 yes | cp $cwd/fail2ban/iptables-multiport.conf /etc/fail2ban/action.d/iptables-multiport.conf
 service fail2ban restart
 log "Installed and configured fail2ban. May they come." INFO
 # SELinux
-echo "Install SELinux for increased security? This will require advanced configuration [y/n]: "
-read iSELinux
-if [[ $iSELinux = "y" ]]; then
+confirm "Install SELinux for increased security? This will require advanced configuration"
+if [[ $confirmed = true ]]; then
   systemctl stop apparmor
   systemctl disable apparmor
-  apt install policycoreutils selinux-basics selinux-utils -y
-  selinux-activate
+  apt install policycoreutils selinux-basics selinux-utils -y &> $aptlogloc
+  selinux-activate &> $cmdlogloc
   if ! [[ $(getenforce) =~ "Disabled" ]]; then
     log "SELinux is not ready to work yet. Something went wrong while activating" ERROR
   fi
@@ -179,11 +219,10 @@ fi
 ###
 # Third party software
 ###
-echo "Type in the corresponding numbers for third party software to install (separated by comma)"
-echo "1) NGINX\\n2)Docker\\n3)Webmin\\n"
+printf "Type in the corresponding numbers for third party software to install (separated by comma)"
+printf "\n1) NGINX\n2) Docker\n3) Webmin\n"
 read itps
-toinstall =$(echo $itps | tr "," "\n")
-
+toinstall=$(echo $itps | tr "," "\n")
 for software in $toinstall
 do
   case $software in
