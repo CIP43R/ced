dd7fded081e2de5cd6f56ead5f1f13ede18117b1
Justin Falkenstein <justin.falkenstein@outlook.com>
2023-01-18 15:56:52 +0100

    fix(install): sudo git

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install git -y
 cd /opt
-git clone https://github.com/CIP43R/setup-linux-server.git
+sudo git clone https://github.com/CIP43R/setup-linux-server.git
 cd setup-linux-server
 sudo bash ./init.sh
