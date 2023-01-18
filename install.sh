297e8009fdc46e48fb1d25efe49cf1e6ef39fa86
Justin Falkenstein <justin.falkenstein@outlook.com>
2023-01-18 15:55:06 +0100

    fix(install): sigh

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-cwd=$(pwd)
-logloc=$cwd/logs/install.log # Custom logs produced in this file
-aptlogloc=$cwd/logs/apt.log  # Logs produced by apt 
-cmdlogloc=$cwd/logs/cmd.log  # Logs produced by applications run in here
-
-printf "[INFO] Starting installation process\\n" | tee -a $logloc
-sudo apt install git -y &> $aptlogloc
+sudo apt install git -y
 cd /opt
-git clone https://github.com/CIP43R/setup-linux-server.git &> $cmdlogloc
+git clone https://github.com/CIP43R/setup-linux-server.git
 cd setup-linux-server
 sudo bash ./init.sh
