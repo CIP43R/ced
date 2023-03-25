6958571722404bf99e8672f947830cbfe0d5c283
Justin Falkenstein <justin.falkenstein@outlook.com>
2023-03-25 23:03:32 +0100

    feat(install): install process
    add chmod to install script
    fixed directory for install
    remove install script to prevent unwanted path inclusion
    remove empty logs and use gitkeep instead

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt install git -y
-sudo git clone https://github.com/CIP43R/setup-linux-server.git /opt/ced
-sudo cp /opt/ced/config.template /opt/ced/config.conf 
-echo export PATH="$PATH:~/shared/setup-linux-server" >> ~/.bashrc
+sudo git clone https://github.com/CIP43R/ced.git /opt/ced
+echo export PATH="$PATH:/opt/ced" >> ~/.bashrc
 source ~/.bashrc
+sudo chmod +x /opt/ced/ced
+rm /opt/ced/install.sh
 
 printf "Installed successfully! You can now type in 'ced' into your terminal to try it!"
